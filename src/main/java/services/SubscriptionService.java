5457ee71007643720a5113153186a7735bb9b8a6
aifia105 <mohamedaifia34@gmail.com>
2025-04-26 11:32:10 +0100

    SubscriptionModule/PaymentModule

---
+++ src/main/java/services/SubscriptionService.java
@@ -0,0 +1,32 @@
+package services;
+
+import entities.Subscription;
+
+import java.util.List;
+
+public class SubscriptionService implements  CRUD<Subscription>{
+    @Override
+    public void ajouter(Subscription subscription) {
+
+    }
+
+    @Override
+    public void supprimer(Subscription subscription) {
+
+    }
+
+    @Override
+    public void modifier(Subscription subscription) {
+
+    }
+
+    @Override
+    public Subscription afficher(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Subscription> afficher_tout() {
+        return List.of();
+    }
+}
